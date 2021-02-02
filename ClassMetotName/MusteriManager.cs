5d19994f951d69d53bda5df5a073792f35a5d629
Yaren <avote841@gmail.com>
2021-02-02 22:51:34 +0300

    proje oluşturuldu

---
+++ ClassMetotName/MusteriManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void MusteriEkle(Musteri musteriler)
+        {
+
+            Console.WriteLine("Müşteri ID sisteme eklendi : " + musteriler.Id);
+            Console.WriteLine("Müşteri adı sisteme eklendi: " + musteriler.Ad);
+            Console.WriteLine("Müşteri soyadı sisteme eklendi: " + musteriler.Soyad);
+        }
+        public void Listeleme(Musteri musteriler)
+        {
+
+            Console.WriteLine("Müşteri ID :" + musteriler.Id);
+            Console.WriteLine("Müşteri adı:" + musteriler.Ad);
+            Console.WriteLine("Müşteri soyadı:" + musteriler.Soyad);
+
+        }
+        public void MusteriSilme(Musteri musteriler)
+        {
+            Console.WriteLine("Id : " + musteriler.Id + " Ad : " + musteriler.Ad + " Soyad : " + musteriler.Soyad + "Sistemden silindi");
+        }
+
+    }
+}
---
+++ ClassMetotName/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 1;
+            musteri1.Ad = "Yaren";
+            musteri1.Soyad = "Zengin";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 2;
+            musteri2.Ad = "Şevval";
+            musteri2.Soyad = "Dönmez";
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Id = 3;
+            musteri3.Ad = "Elif";
+            musteri3.Soyad = "Akyol ";
+
+            Musteri musteri4 = new Musteri();
+            musteri4.Id = 4;
+            musteri4.Ad = "Nejdet";
+            musteri4.Soyad = "Yılmaz ";
+
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.MusteriEkle(musteri4);
+
+
+            Console.WriteLine("-------Müşteri Listesi--------");
+            musteriManager.Listeleme(musteri1);
+            Console.WriteLine("---------------------------");
+            musteriManager.Listeleme(musteri2);
+            Console.WriteLine("---------------------------");
+            musteriManager.Listeleme(musteri3);
+            Console.WriteLine("---------------------------");
+            musteriManager.Listeleme(musteri4);
+            Console.WriteLine("---------------------------");
+
+            musteriManager.MusteriSilme(musteri3);
+        }
+
+
+
+
+    }
+}
